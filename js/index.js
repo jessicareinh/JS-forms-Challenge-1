a0434edc8ca69b4821787fae7cca0ac4c35a4961
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2023-12-12 21:38:59 +0100

    add solutions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); //verhindert das Standardverhalten des Formulars
+
+const formData = new formData(event.target);
+const data = Object.fromEntries(formData);
+
+console.log(data);
+
+const firstName = data.firstName;
+const age = Number(data.age);
+const badness = Number(data.badness);
+
+console.log(`The age-badness-sum of ${firstName} is ${age+badness}`);
+
+event.target.reset ();
+event.target.elements.firstName.focus();
+});
